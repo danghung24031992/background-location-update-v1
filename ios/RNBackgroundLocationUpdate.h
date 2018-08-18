24849f049c0954f96c3d78df4af8ccb3b5eea494
hungnd <danghung24031992@gmail.com>
2018-08-18 10:29:31 +0700

    background location for ios done

--- ios/RNBackgroundLocationUpdate.h
+++ ios/RNBackgroundLocationUpdate.h
@@ -1,11 +1,26 @@
+//#if __has_include("RCTBridgeModule.h")
+//    #import "RCTBridgeModule.h"
+//#else
+//    #import <React/RCTBridgeModule.h>
+//#endif
 
-#if __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
+#if __has_include(<React/RCTBridgeModule.h>)
+    #import <React/RCTBridgeModule.h>
 #else
-#import <React/RCTBridgeModule.h>
+    #import "RCTBridgeModule.h"
 #endif
 
-@interface RNBackgroundLocationUpdate : NSObject <RCTBridgeModule>
 
+#if __has_include(<React/RCTEventEmitter.h>)
+    #import <React/RCTEventEmitter.h>
+#else
+    #import "RCTEventEmitter.h"
+#endif
+
+#import "LocationTracker.h"
+
+@interface RNBackgroundLocationUpdate : RCTEventEmitter <RCTBridgeModule>
+@property LocationTracker * locationTracker;
+@property (nonatomic) NSTimer* locationUpdateTimer;
 @end
   
--- ios/RNBackgroundLocationUpdate.m
+++ ios/RNBackgroundLocationUpdate.m
@@ -1,7 +1,12 @@
 
 #import "RNBackgroundLocationUpdate.h"
+#import <UIKit/UIKit.h>
 
 @implementation RNBackgroundLocationUpdate
+
+- (NSArray<NSString *> *)supportedEvents {
+    return @[@"didUpdateLocation"];
+}
 
 - (dispatch_queue_t)methodQueue
 {
@@ -9,5 +14,55 @@
 }
 RCT_EXPORT_MODULE()
 
+RCT_EXPORT_METHOD(locationDidUpdate)
+{
+    NSLog(@"locationDidUpdate");
+    UIAlertView * alert;
+    
+    //We have to make sure that the Background App Refresh is enable for the Location updates to work in the background.
+    if([[UIApplication sharedApplication] backgroundRefreshStatus] == UIBackgroundRefreshStatusDenied){
+        
+//        alert = [[UIAlertView alloc]initWithTitle:@""
+//                                          message:@"The app doesn't work without the Background App Refresh enabled. To turn it on, go to Settings > General > Background App Refresh"
+//                                         delegate:nil
+//                                cancelButtonTitle:@"Ok"
+//                                otherButtonTitles:nil, nil];
+//        [alert show];
+        
+    }else if([[UIApplication sharedApplication] backgroundRefreshStatus] == UIBackgroundRefreshStatusRestricted){
+        
+//        alert = [[UIAlertView alloc]initWithTitle:@""
+//                                          message:@"The functions of this app are limited because the Background App Refresh is disable."
+//                                         delegate:nil
+//                                cancelButtonTitle:@"Ok"
+//                                otherButtonTitles:nil, nil];
+//        [alert show];
+    } else{
+        
+        self.locationTracker = [[LocationTracker alloc]init];
+        [self.locationTracker startLocationTracking];
+        
+        //Send the best location to server every 30 seconds
+        //You may adjust the time interval depends on the need of your app.
+        NSTimeInterval time = 30.0;
+        
+        self.locationUpdateTimer = [NSTimer scheduledTimerWithTimeInterval:time
+                                                                    target:self
+                                                                  selector:@selector(updateLocation)
+                                                                  userInfo:nil
+                                                                   repeats:YES];
+    }
+}
+
+-(void)updateLocation {
+    NSLog(@"didUpdateLocation");
+    [self.locationTracker updateLocationToServer:^(NSDictionary * location) {
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.2 * NSEC_PER_SEC),
+                       dispatch_get_main_queue(), ^(void){
+                           [self sendEventWithName:@"didUpdateLocation" body:location];
+                       });
+    }];
+}
+
 @end
-  
+
